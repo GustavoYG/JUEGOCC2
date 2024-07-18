5869d933e58f24597e9c0132d9993ef0c436c5fe
mrcscomputer <maceituno@unsa.edu.pe>
2024-07-18 10:14:10 -0500

    Tank.h

--- Tank.h
+++ Tank.h
@@ -3,11 +3,12 @@
 
 #include <SFML/Graphics.hpp>
 #include "Bullet.h"
+#include "Mine.h"
 #include <memory>
 
 const int MAP_WIDTH = 20;
 const int MAP_HEIGHT = 15;
-const int TILE_SIZE = 40; // Definir TILE_SIZE aquí
+const int TILE_SIZE = 40; // Definir TILE_SIZE aquÃ­
 
 class Tank {
 public:
@@ -15,14 +16,22 @@ public:
 
     void moveForward(float deltaTime, const sf::Vector2u& windowSize, const int map[MAP_HEIGHT][MAP_WIDTH]);
     void moveBackward(float deltaTime, const sf::Vector2u& windowSize, const int map[MAP_HEIGHT][MAP_WIDTH]);
+    void moveRight(float deltaTime, const sf::Vector2u& windowSize, const int map[MAP_HEIGHT][MAP_WIDTH]);
+    void moveLeft(float deltaTime, const sf::Vector2u& windowSize, const int map[MAP_HEIGHT][MAP_WIDTH]);
+
     void rotate(float angle);
 
+    std::unique_ptr<Mine> placeMine(const sf::Texture& mineTexture);
+
+    
     std::unique_ptr<Bullet> shoot();
     sf::RectangleShape getShape();
     sf::Vector2f getPosition() const;
+    void setPosition(float, float);
 
 private:
     sf::RectangleShape shape;
+    std::vector<std::unique_ptr<Mine>> mines;
     float speed;
 
     bool isWithinWindow(const sf::Vector2f& position, const sf::Vector2u& windowSize);
