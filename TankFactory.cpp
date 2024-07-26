5ad7bdd750ce2010e10ad74c79af9f9be7b4d672
Roberto Gustavo Yavar Guillen <104165295+GustavoYG@users.noreply.github.com>
2024-07-26 08:50:43 -0500

    Delete TankFactory.cpp

--- TankFactory.cpp
+++
@@ -1,5 +0,0 @@
-#include "TankFactory.h"
-
-std::unique_ptr<Tank> TankFactory::createTank(sf::Vector2f position, const sf::Texture& texture) {
-    return std::make_unique<Tank>(position, texture);
-}
